48fd969c4bfc82266b66198331a53703bcaff7e1
yevhenii-telychko <yevgeniy2004@gmail.com>
2022-10-26 12:22:16 +0200

    add game factory

---
+++ lab_1/GameController.cs
@@ -0,0 +1,20 @@
+﻿namespace lab_1
+{
+    class GameController
+    {
+        public Game getBasicGame()
+        {
+            return new BasicGame();
+        }
+
+        public Game getRankedGame()
+        {
+            return new RankedGame();
+        }
+
+        public Game getSafeGame()
+        {
+            return new SafeGame();
+        }
+    }
+}
--- lab_1/Program.cs
+++ lab_1/Program.cs
@@ -7,13 +7,14 @@ class Program
         UpgradedGameAccount Ihor = new UpgradedGameAccount("Ihor_Obnal");
         PremiumGameAccount Danila = new PremiumGameAccount("Danila_Glovo");
 
-        BasicGame BasicGame = new BasicGame();
-        RankedGame RankedGame = new RankedGame();
-        SafeGame SafeGame = new SafeGame();
+        GameController gameController = new GameController();
 
-        BasicGame.PlayGame(Ihor, Danila);
-        RankedGame.PlayGame(Ihor, Danila);
-        SafeGame.PlayGame(Ihor, Danila);
+        List<Game> games = new List<Game> { gameController.getBasicGame(), gameController.getRankedGame(), gameController.getSafeGame() };
+
+        foreach (var game in games)
+        {
+            game.PlayGame(Danila, Ihor);
+        }
 
         Console.Write("Ihor_Obnal's rating:");
         Console.WriteLine(Ihor.CurrentRating);
